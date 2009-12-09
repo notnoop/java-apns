de16efcbc69186571cd7dcd14d1bc63eb3655e64
notnoop <mahmood@notnoop.com>
2009-12-09 01:35:52 -0500

    add tests for the queued service

--- src/test/java/com/notnoop/apns/internal/QueuedApnsServiceTest.java
+++ src/test/java/com/notnoop/apns/internal/QueuedApnsServiceTest.java
@@ -1,7 +1,10 @@
 package com.notnoop.apns.internal;
 
+import static org.junit.Assert.*;
 import org.junit.Test;
+import static org.mockito.Mockito.*;
 
+import com.notnoop.apns.ApnsNotification;
 import com.notnoop.apns.ApnsService;
 
 public class QueuedApnsServiceTest extends ApnsServiceImplTest{
@@ -10,6 +13,30 @@ public class QueuedApnsServiceTest extends ApnsServiceImplTest{
     public void sendWithoutStarting() {
         QueuedApnsService service = new QueuedApnsService(null);
         service.push(notification);
+    }
+
+    @Test
+    public void dontBlock() {
+        final int delay = 10000;
+        ApnsConnection connection = new ApnsConnection(null, null, 0) {
+            protected synchronized void sendMessage(ApnsNotification m) {
+                try {
+                    Thread.sleep(delay);
+                } catch (InterruptedException e) {
+                }
+            }
+        };
+
+        QueuedApnsService queued =
+            new QueuedApnsService(new ApnsServiceImpl(connection, null));
+        queued.start();
+        long time1 = System.currentTimeMillis();
+        queued.push(notification);
+        queued.push(notification);
+        long time2 = System.currentTimeMillis();
+        assertTrue("Queued blocks unexpectedly", (time2- time1) < delay);
+
+        queued.stop();
     }
 
     protected ApnsService newService(ApnsConnection connection, ApnsFeedbackConnection feedback) {
