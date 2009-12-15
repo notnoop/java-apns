dc213c10f3a9394d2530a00aecce91a9261f983f
Mahmood Ali <mahmood@notnoop.com>
2009-12-14 19:35:06 -0500

    more coverage tests

---
+++ src/test/java/com/notnoop/apns/internal/SillyTests.java
@@ -0,0 +1,45 @@
+package com.notnoop.apns.internal;
+
+import java.util.Collections;
+import java.util.Date;
+import java.util.Map;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class SillyTests {
+
+    @Test
+    public void testFeedbackCalls() {
+        Map<String, Date> map = Collections.singletonMap("Test", new Date());
+        ApnsFeedbackConnection feed = mock(ApnsFeedbackConnection.class);
+        when(feed.getInactiveDevices()).thenReturn(map);
+
+        ApnsServiceImpl service = new ApnsServiceImpl(null, feed);
+        assertEquals(map, service.getInactiveDevices());
+        
+        // The feedback should be called once at most
+        // Otherwise, we need to verify that the results of both
+        // calls are accounted for
+        verify(feed, times(1)).getInactiveDevices();
+    }
+
+    @Test
+    public void testQueuedFeedbackCalls() {
+        Map<String, Date> map = Collections.singletonMap("Test", new Date());
+        ApnsFeedbackConnection feed = mock(ApnsFeedbackConnection.class);
+        when(feed.getInactiveDevices()).thenReturn(map);
+
+        ApnsServiceImpl service = new ApnsServiceImpl(null, feed);
+        QueuedApnsService queued = new QueuedApnsService(service);
+        assertEquals(map, queued.getInactiveDevices());
+
+        // The feedback should be called once at most
+        // Otherwise, we need to verify that the results of both
+        // calls are accounted for
+        verify(feed, times(1)).getInactiveDevices();
+    }
+
+}
