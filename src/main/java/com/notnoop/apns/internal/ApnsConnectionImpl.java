63469cb89950cdeef95b7adf9e47193c8e817802
Frohwalt Egerer <mail@frohwalt.de>
2012-11-22 13:36:49 +0100

    Close testConnection even if there is an exception

--- src/main/java/com/notnoop/apns/internal/ApnsConnectionImpl.java
+++ src/main/java/com/notnoop/apns/internal/ApnsConnectionImpl.java
@@ -210,8 +210,13 @@ public class ApnsConnectionImpl implements ApnsConnection {
     }
 
     public void testConnection() throws NetworkIOException {
-        ApnsConnectionImpl testConnection = new ApnsConnectionImpl(factory, host, port, reconnectPolicy.copy(), ApnsDelegate.EMPTY);
-        testConnection.sendMessage(new SimpleApnsNotification(new byte[] {0}, new byte[]{0}));
-        testConnection.close();
+        ApnsConnectionImpl testConnection = null;
+        try {
+            testConnection =new ApnsConnectionImpl(factory, host, port, reconnectPolicy.copy(), ApnsDelegate.EMPTY);
+            testConnection.sendMessage(new SimpleApnsNotification(new byte[] {0}, new byte[]{0}));
+        } finally {
+            if (testConnection != null)
+                testConnection.close();
+        }
     }
 }
