de83b4fd062c7c90e2b127a6ec53939539c4647b
Karl Moore <kmoore@barracuda.com>
2013-06-12 14:21:55 +0100

    abstract class should be public to allow extension without having to repeat boilerplate methods

--- src/test/java/com/notnoop/apns/ApnsGatewayServerSocket.java
+++ src/test/java/com/notnoop/apns/ApnsGatewayServerSocket.java
@@ -5,7 +5,6 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.Socket;
 import java.util.concurrent.ExecutorService;
 
